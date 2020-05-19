c9a37f12a69998001669d7023d3c3318f8781bb9
jhayes00 <jason.hayes2@gmail.com>
2020-05-19 19:41:10 -0400

    commit then pull origin master

---
+++ app/controllers/api/v1/favorite_recipes_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::FavoriteRecipesController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
+  def create
+    new_favorite_recipe = FavoriteRecipe.new(recipe_id: params["recipe_id"])
+    new_favorite_recipe.user = current_user
+    if new_favorite_recipe.save
+      flash.now[:notice] = "Recipe added to favorites!"
+    else
+      flash.now[:notice] = new_favorite_recipe.errors.full_messages
+    end
+  end
+end
