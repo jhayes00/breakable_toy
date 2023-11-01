83a8a8ff540b972622b0e4573612ed63c7497dae
jhayes00 <jason.hayes2@gmail.com>
2023-11-01 16:27:58 -0400

    recipe list cleanup

--- app/javascript/react/components/RecipeTile.js
+++ app/javascript/react/components/RecipeTile.js
@@ -1,26 +1,18 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const RecipeTile = props => {
-  const id = props.id
-  const title = props.title
-  const image = props.image
-  const numLikes = props.numLikes
-  const numMissedIngredients = props.numMissedIngredients
-
-  return (
-    <div className='cell small-12 medium-4 text-center recipe-tile'>
-      <Link to={`/recipes/${id}`}>
-        <img
-          src={image}
-          alt="image"
-        />
-        <h5>{title}</h5>
-      </Link>
-      Likes: {numLikes} <br />
-      Missing Ingredients: {numMissedIngredients}
-    </div>
-  )
-}
+const RecipeTile = recipe => (
+  <div className='cell small-12 medium-4 text-center recipe-tile'>
+    <Link to={`/recipes/${recipe.id}`}>
+      <img
+        src={recipe.image}
+        alt="image"
+      />
+      <h5>{recipe.title}</h5>
+    </Link>
+    Likes: {recipe.numLikes} <br />
+    Missing Ingredients: {recipe.numMissedIngredients}
+  </div>
+)
 
 export default RecipeTile
